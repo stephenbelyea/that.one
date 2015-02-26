8765565eba49dc4158d86933c5bb87a9d69f5ffc
Stephen Belyea <belyea@farenagency.com>
2015-02-25 21:13:07 -0500

    API pulls with key in place, working on pulling key from config object.

--- js/global.js
+++ js/global.js
@@ -1,28 +1,19 @@
 jQuery(document).ready(function($){
-	// Grab window dimensions and scroll position on load.
-	var $win = do_the_sizes();
-	// Listen for resizing.
-	$(window).on('resize',function(){ 
-		$win = do_the_sizes(); 
-	});
-	// Listen for scrolling. 
-	$(document).on('scroll',function(){ 
-		$win.s = do_the_scroll(); 
+
+	$.getJSON(config,function(data){ 
+		tmdb_api = data.tmdb_api; 
+		console.log(tmdb_api);
 	});
 
-	// Make with the JS.
-	console.log('Do it!');
+	console.log(tmdb_api);
+
+
+	var search_str = "http://api.themoviedb.org/3/search/movie?api_key="+tmdb_api.key+"&query=birdman"; 
+	$.getJSON(search_str,function(data){
+		//console.log(data);
+	});
+	
 });
 
-// Return scroll position int.
-function do_the_scroll(){ 
-	return $(document).scrollTop(); 
-}
-// Return sizing / scroll position as object values. 
-function do_the_sizes(){ 
-	return {
-		'w' : $(window).width(),
-		'h' : $(window).height(),
-		's' : do_the_scroll()
-	}; 
-}
+var config = "./config.json",
+	tmdb_api;
